955807a41669e4c1914165094bf3131d8f33d5eb
郭沈厅 <washingtin@sina.com>
2015-04-16 13:34:53 +0800

    删除pom.xml

--- src/main/resources/sql/mongo.sql
+++
@@ -1,3 +0,0 @@
-db.ticketInfo.ensureIndex({'ticket_no':1,'coupon_info.coupon_no':1});
-db.ticketInfo.ensureIndex({'coupon_info.coupon_no':1});
-db.ticketInfo.ensureIndex({'status':1,'coupon_info.coupon_no':1});
--- src/main/webapp/WEB-INF/errorpage/405.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>405</h2>
-</body>
-</html>
